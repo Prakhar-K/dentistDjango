2b69e386a937e633bcf3a7db07dc1f983eb07943
Prakhar Katiyar <prakhar.working92@gmail.com>
2021-01-09 09:24:33 +0530

    added template to settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'Templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'dentist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,3 +140,4 @@ EMAIL_USE_TLS = False
 
 
 django_heroku.settings(locals())
+
